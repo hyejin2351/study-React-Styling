5e11ddab3e2de031d7d256cfde7110a58a9379e7
hyejin Kim <hyejin2351@gmail.com>
2018-11-21 17:33:55 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
-import styles from './App.css';
-import classNames from 'classnames'
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-      <div className={classNames(styles.box, styles.blue)}>
-      
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
