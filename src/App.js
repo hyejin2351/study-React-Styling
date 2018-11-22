f6166f753710e64afbd8b5c9e9b27676ae29127b
hyejin Kim <hyejin2351@gmail.com>
2018-11-22 14:18:35 +0900

    styled-component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
-import SassComponent from "./SassComponent";
+import Button from './components/Button'
+import StyledButton from './components/StyledButton'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <SassComponent/>
+        <Button>Sass 버튼</Button>
+        <StyledButton big>스타일드 버튼</StyledButton>
       </div>
     );
   }
