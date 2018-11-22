76d29dccf30df897288ebabdb3614728184ec20a
hyejin Kim <hyejin2351@gmail.com>
2018-11-22 11:43:32 +0900

    신규 버전 실습 환경 세팅

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import styles from './App.scss';
-import classNames from 'classnames/bind';
-
-const blueBox = classNames.bind(styles);
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
-    const isBlue = true;
-    
     return (
-      <div className={blueBox('box', {
-        blue: isBlue
-        })}>
-        <div className={blueBox('box-inside')}></div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
